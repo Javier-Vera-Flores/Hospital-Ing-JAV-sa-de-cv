42a855d9098d503f4ff07f9f443662894f3f4da3
Javier Vera Flores <aczayacatl27@hotmail.com>
2025-01-01 19:24:23 -0600

    Corriendo links y login

--- Cliente/scripts/login.js
+++ Cliente/scripts/login.js
@@ -1,81 +1,86 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Asignar el evento 'submit' al formulario de login
-    const loginForm = document.getElementById('loginForm');
-    loginForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
+//const SERVER_URL = "http://127.0.0.1:3000"; // URL del servidor
+const SERVER_URL = "http://127.0.0.1:3000"; // URL del servidor
+document.addEventListener("DOMContentLoaded", () => {
+  // Asignar el evento 'submit' al formulario de login
+  const loginForm = document.getElementById("loginForm");
+  loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-        // Obtener valores del formulario
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+    // Obtener valores del formulario
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-        // Llamar a la función de inicio de sesión
-        await iniciarSesion(username, password);
-    });
+    // Llamar a la función de inicio de sesión
+    await iniciarSesion(username, password);
+  });
 
-    // Asignar el evento 'submit' al formulario de registro
-    const registerForm = document.getElementById('registerForm');
-    registerForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
+  // Asignar el evento 'submit' al formulario de registro
+  const registerForm = document.getElementById("registerForm");
+  registerForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-        // Obtener valores del formulario
-        const newUsername = document.getElementById('newUsername').value;
-        const newPassword = document.getElementById('newPassword').value;
+    // Obtener valores del formulario
+    const newUsername = document.getElementById("newUsername").value;
+    const newPassword = document.getElementById("newPassword").value;
 
-        // Llamar a la función de registro
-        await agregarUsuario(newUsername, newPassword);
-    });
+    // Llamar a la función de registro
+    await agregarUsuario(newUsername, newPassword);
+  });
 });
 
 // Función de inicio de sesión
 async function iniciarSesion(username, password) {
-    try {
-        const response = await fetch('/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password }),
-        });
+  try {
+    //    const response = await fetch("/login", {
+    const response = await fetch(`${SERVER_URL}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
 
-        const result = await response.json();
-        const messageElement = document.getElementById('loginMessage');
-        if (result.success) {
-            messageElement.textContent = 'Inicio de sesión exitoso';
-            messageElement.style.color = 'green';
+    const result = await response.json();
+    const messageElement = document.getElementById("loginMessage");
+    if (result.success) {
+      messageElement.textContent = "Inicio de sesión exitoso";
+      messageElement.style.color = "green";
 
-            // Guardar el nombre de usuario en localStorage
-            localStorage.setItem('loggedInUser', username);
+      // Guardar el nombre de usuario en localStorage
+      localStorage.setItem("loggedInUser", username);
 
-            window.location.href = 'inicio.html';
-        } else {
-            messageElement.textContent = 'Usuario o contraseña incorrectos';
-            messageElement.style.color = 'red';
-        }
-    } catch (error) {
-        console.error('Error al procesar el login:', error);
-        alert('Hubo un problema al conectar con el servidor.');
+      //window.location.href = 'inicio.html';
+      window.location.href = "../index.html";
+    } else {
+      messageElement.textContent = "Usuario o contraseña incorrectos";
+      messageElement.style.color = "red";
     }
+  } catch (error) {
+    console.error("Error al procesar el login:", error);
+    alert("Hubo un problema al conectar con el servidor.");
+  }
 }
-
 
 // Función de registro
 async function agregarUsuario(newUsername, newPassword) {
-    try {
-        const response = await fetch('/register', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: newUsername, password: newPassword }),
-        });
+  try {
+    //const response = await fetch("/register", {
+    const response = await fetch(`${SERVER_URL}/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: newUsername, password: newPassword }),
+    });
 
-        const result = await response.json();
-        const messageElement = document.getElementById('registerMessage');
-        if (result.success) {
-            messageElement.textContent = 'Usuario registrado exitosamente';
-            messageElement.style.color = 'green';
-        } else {
-            messageElement.textContent = 'Error al registrar usuario: ' + result.message;
-            messageElement.style.color = 'red';
-        }
-    } catch (error) {
-        console.error('Error al procesar el registro:', error);
-        alert('Hubo un problema al conectar con el servidor.');
+    const result = await response.json();
+    const messageElement = document.getElementById("registerMessage");
+    if (result.success) {
+      messageElement.textContent = "Usuario registrado exitosamente";
+      messageElement.style.color = "green";
+    } else {
+      messageElement.textContent =
+        "Error al registrar usuario: " + result.message;
+      messageElement.style.color = "red";
     }
+  } catch (error) {
+    console.error("Error al procesar el registro:", error);
+    alert("Hubo un problema al conectar con el servidor.");
+  }
 }
--- Servidor/server.js
+++ Servidor/server.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const soap = require('soap'); //Creo que se va a usar
-const http = require('http');
-require('dotenv').config(); // Manejar variables de entorno
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const soap = require("soap"); //Creo que se va a usar
+const http = require("http");
+require("dotenv").config(); // Manejar variables de entorno
 
 const app = express();
 
-const cors = require('cors'); // Importar el paquete
+const cors = require("cors"); // Importar el paquete
 
 // Configurar CORS
-app.use(cors({
-    origin: 'http://127.0.0.1:3000', // Cambia esto al origen donde está tu cliente
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type'], // Encabezados permitidos
-}));
-
+app.use(
+  cors({
+    // origin: "http://127.0.0.1:3000", // Cambia esto al origen donde está tu cliente
+    //Si quieres que sea desde cualquier origin sustituy origin por --> origin: '*'
+    origin: ["http://127.0.0.1:3000", "http://127.0.0.1:51346"], // Cambia esto al origen donde está tu cliente
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type"], // Encabezados permitidos
+  })
+);
 
 // Configuración de host y puerto
-const HOST = process.env.HOST || '127.0.0.1';
+const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 3000;
 
 // Middleware para interpretar JSON en los requests
@@ -26,78 +29,95 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Servir archivos estáticos de la carpeta "public"
-app.use(express.static(path.join(__dirname, '../Cliente')));
+app.use(express.static(path.join(__dirname, "../Cliente")));
 
 // Rutas REST para login y registro
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'Error al leer el archivo de usuarios' });
-        }
+  fs.readFile(path.join(__dirname, "users.json"), "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Error al leer el archivo de usuarios",
+      });
+    }
 
-        const users = JSON.parse(data);
-        const user = users.find(user => user.username === username && user.password === password);
+    const users = JSON.parse(data);
+    const user = users.find(
+      (user) => user.username === username && user.password === password
+    );
 
-        if (user) {
-            res.json({ success: true });
-        } else {
-            res.json({ success: false, message: 'Credenciales incorrectas' });
-        }
-    });
+    if (user) {
+      res.json({ success: true });
+    } else {
+      res.json({ success: false, message: "Credenciales incorrectas" });
+    }
+  });
 });
 
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
 
-    fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, "users.json"), "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Error al leer el archivo de usuarios",
+      });
+    }
+
+    const users = JSON.parse(data);
+    const userExists = users.some((user) => user.username === username);
+
+    if (userExists) {
+      return res.json({ success: false, message: "El usuario ya existe" });
+    }
+
+    users.push({ username, password });
+
+    fs.writeFile(
+      path.join(__dirname, "users.json"),
+      JSON.stringify(users, null, 2),
+      (err) => {
         if (err) {
-            return res.status(500).json({ success: false, message: 'Error al leer el archivo de usuarios' });
+          return res
+            .status(500)
+            .json({ success: false, message: "Error al guardar el usuario" });
         }
 
-        const users = JSON.parse(data);
-        const userExists = users.some(user => user.username === username);
-
-        if (userExists) {
-            return res.json({ success: false, message: 'El usuario ya existe' });
-        }
-
-        users.push({ username, password });
-
-        fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users, null, 2), (err) => {
-            if (err) {
-                return res.status(500).json({ success: false, message: 'Error al guardar el usuario' });
-            }
-
-            res.json({ success: true });
-        });
-    });
+        res.json({ success: true });
+      }
+    );
+  });
 });
 
 // Ruta para servir el archivo de doctores
-app.get('/doctores', (req, res) => {
-    const filePath = path.join(__dirname, 'doctores.json');
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: "Error al leer el archivo de doctores" });
-        }
-        res.json(JSON.parse(data));
-    });
+app.get("/doctores", (req, res) => {
+  const filePath = path.join(__dirname, "doctores.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Error al leer el archivo de doctores" });
+    }
+    res.json(JSON.parse(data));
+  });
 });
 
 // Ruta para inicializar en localhost 3000 en inicio.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../Cliente', 'inicio.html'));
+app.get("/", (req, res) => {
+  //res.sendFile(path.join(__dirname, '../Cliente', 'inicio.html'));
+  res.sendFile(path.join(__dirname, "../Cliente", "index.html"));
 });
 
 /******************************
- * INICIO - Contactanos 
+ * INICIO - Contactanos
  *****************************/
-// No se puede en backend 
+// No se puede en backend
 
 /******************************
- * FIN - Contactanos 
+ * FIN - Contactanos
  *****************************/
 
 // Crear el servidor HTTP para integrar ambos servicios
@@ -105,5 +125,5 @@ const server = http.createServer(app);
 
 // Iniciar el servidor
 server.listen(PORT, HOST, () => {
-    console.log(`Servidor REST ejecutándose en http://${HOST}:${PORT}`);
+  console.log(`Servidor REST ejecutándose en http://${HOST}:${PORT}`);
 });
