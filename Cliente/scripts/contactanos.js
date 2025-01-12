054e4c47d7b30b64f1c9cde19dbcd1f6658afed3
José Vicente López López <al2212002118@azc.uam.mx>
2025-01-11 22:05:53 -0600

    Icono de mapa

--- Cliente/scripts/contactanos.js
+++ Cliente/scripts/contactanos.js
@@ -37,18 +37,6 @@ function enviarCorreo(nombre, correo, mensaje) {
     });
 }
 
-function iniciarMap(){
-    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
-
 //Configuración dinámica del boton de iniciar/cerrar sesion
 document.addEventListener('DOMContentLoaded', () => {
     const userGreeting = document.getElementById('userGreeting');
